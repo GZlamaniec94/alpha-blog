69f6aa50ee850ecc13622719b3d7a2b8af6eea54
Grzegorz Złamaniec <g.zlamaniec@gmail.com>
2022-03-08 16:41:41 +0100

    Added update and edit methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,10 @@ class ArticlesController < ApplicationController
     def new
         @article = Article.new
     end
+
+    def edit
+        @article = Article.find(params[:id])        
+    end    
     
     def create
         @article = Article.new(params.require(:article).permit(:title, :description))
@@ -20,5 +24,15 @@ class ArticlesController < ApplicationController
         else
             render 'new'
         end    
-    end                
+    end
+    
+    def update
+        @article = Article.find(params[:id])
+        if  @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article was updated successfully."
+            redirect_to @article
+        else    
+            render 'edit'
+        end
+    end                        
 end    
