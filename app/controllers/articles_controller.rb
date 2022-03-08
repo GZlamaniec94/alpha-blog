301d1c2a4056edc41723bb3edc53ff13b8e6c066
Grzegorz Złamaniec <g.zlamaniec@gmail.com>
2022-03-08 15:09:16 +0100

    Added create page and function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,18 @@ class ArticlesController < ApplicationController
     def show    
         @article = Article.find(params[:id])
     end
+    
     def index
         @articles = Article.all
-    end        
+    end
+    
+    def new
+
+    end
+    
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article.save
+        redirect_to @article
+    end                
 end    
