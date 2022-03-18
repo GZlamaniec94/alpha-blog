557c1dd8b475756f335c1eaa0dffa04c6c8ae95c
Grzegorz Złamaniec <g.zlamaniec@gmail.com>
2022-03-18 16:56:07 +0100

    Add users create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,21 @@
 class UsersController < ApplicationController
     def new
         @user = User.new
-    end    
+    end
+    
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            flash[:notice] = "Your account was creates successfully."
+            redirect_to articles_path
+        else
+            render 'new'
+        end        
+    end
+    
+    
+    private
+    def user_params
+         params.require(:user).permit(:username, :email, :password)
+    end        
 end    
