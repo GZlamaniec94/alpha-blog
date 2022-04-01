0986e809a405e48d5933d54dfff9d68744237715
Grzegorz Złamaniec <g.zlamaniec@gmail.com>
2022-04-01 12:56:55 +0200

    Added pagination

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,7 @@ class ArticlesController < ApplicationController
     end
     
     def index
-        @articles = Article.all
+        @articles = Article.paginate(page: params[:page], per_page: 2 )
     end
     
     def new
