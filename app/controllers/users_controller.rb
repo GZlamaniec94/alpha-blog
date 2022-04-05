a8b810f8bf08d57f8b6852463abc45108bb9d0e5
Grzegorz Złamaniec <g.zlamaniec@gmail.com>
2022-04-05 15:49:56 +0200

    Added destroy user method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-    before_action :set_user, only: [:show, :edit, :update]
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
     before_action :require_user, only: [:edit, :update]
-    before_action :require_same_user, only: [:edit, :update]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
 
     def show        
         @articles = @user.articles.paginate(page: params[:page], per_page: 3 )   
@@ -36,7 +36,19 @@ class UsersController < ApplicationController
             render 'new'
         end        
     end
+    
+    def destroy
+        @user.destroy
+        session[:user_id] = nil
+        flash[:notice] = "Your account and articles successfull deleted."
+        redirect_to root_path
+    end    
+    
+    
+    
+    
     private   
+    
     def user_params
          params.require(:user).permit(:username, :email, :password)
     end       
