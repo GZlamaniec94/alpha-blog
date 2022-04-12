9824c657d35d76dc7187fbd5001e33bd916954f9
Grzegorz Złamaniec <g.zlamaniec@gmail.com>
2022-04-12 11:03:01 +0200

    Added tests

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
-
-end    
+    validates :name, presence: true
+    validates_uniqueness_of :name
+end        
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,16 +1,25 @@
-require  'test_helper'
+require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
 
-    test "category should be valid" do
-        @category = Category.new(name: "Sports")
-        assert @category.valid?
-    end
+  def setup
+    @category = Category.new(name: "Sports")
+  end
 
-    
-    test "name should be present" do
-        @category = Category.new(name: "s")
-        assert_not @category.valid?
-    end
+  test "category should be valid" do
+    assert @category.valid?
+  end
 
-end    
+  test "name should be present" do
+    @category.name = " "
+    assert_not @category.valid?
+  end
+
+  test "name should be unique" do
+    @category.save
+    @category2 = Category.new(name: "Sports")
+    assert_not @category2.valid?
+  end
+
+
+end
