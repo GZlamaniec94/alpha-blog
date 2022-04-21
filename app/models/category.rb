7628484f35400d42067ef76a76d2e6c4604e1d7d
Grzegorz Złamaniec <g.zlamaniec@gmail.com>
2022-04-21 12:52:10 +0200

    Added new integration test

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
-    validates :name, presence: true, length: {minimum: 3, maximum: 25}
+    validates :name, presence: true,
+        uniqueness: { case_sensitive: false }, 
+        length: {minimum: 3, maximum: 25}
     validates_uniqueness_of :name
 end        
